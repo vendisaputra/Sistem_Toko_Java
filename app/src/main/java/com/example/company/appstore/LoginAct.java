545b863beb40c7422c976d1cb3c16ac3456743d6
vendisaputra <vendisaputra@gmail.com>
2019-10-21 18:42:38 +0700

    add validation login kepala cabang

--- app/src/main/java/com/example/company/appstore/LoginAct.java
+++ app/src/main/java/com/example/company/appstore/LoginAct.java
@@ -44,65 +44,61 @@ public class LoginAct extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //loading
-                btn_login.setEnabled(false);
-                btn_login.setText("Loading...");
+                if (validation() == true) {
+                    //loading
+                    btn_login.setEnabled(false);
+                    btn_login.setText("Loading...");
 
-                //database
-                String username = xusername.getText().toString();
-                final String password = xpass.getText().toString();
+                    //database
+                    String username = xusername.getText().toString();
+                    final String password = xpass.getText().toString();
 
-                reference= FirebaseDatabase.getInstance().getReference().
-                        child("KepalaCabang").child(username);
+                    reference = FirebaseDatabase.getInstance().getReference().
+                            child("KepalaCabang").child(username);
 
-                reference.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        if(dataSnapshot.exists()){
+                    reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
 
-                            // ambil data password dari firbase
-                            String passwordFromFirebase = dataSnapshot.child("password").getValue().toString();
+                                // ambil data password dari firbase
+                                String passwordFromFirebase = dataSnapshot.child("password").getValue().toString();
 
-                            //Validasi password dengan firebase
-                            if(password.equals(passwordFromFirebase)){
-
-
-                                //Simpan username key pada local
-                                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
-                                SharedPreferences.Editor editor = sharedPreferences.edit();
-                                editor.putString(username_key, xusername.getText().toString());
-                                editor.apply();
-
-                                //pindah activity
-                                Intent gotoprofil = new Intent(LoginAct.this,DashbordAct.class);
-                                startActivity(gotoprofil);
+                                //Validasi password dengan firebase
+                                if (password.equals(passwordFromFirebase)) {
 
 
-                            }
-                            else {
-                                Toast.makeText(getApplicationContext(),"Password salah!", Toast.LENGTH_SHORT).show();
+                                    //Simpan username key pada local
+                                    SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString(username_key, xusername.getText().toString());
+                                    editor.apply();
+
+                                    //pindah activity
+                                    Intent gotoprofil = new Intent(LoginAct.this, DashbordAct.class);
+                                    startActivity(gotoprofil);
+
+
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Password salah!", Toast.LENGTH_SHORT).show();
+                                    btn_login.setEnabled(true);
+                                    btn_login.setText("Login");
+                                }
+
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Username tidak ada!", Toast.LENGTH_SHORT).show();
                                 btn_login.setEnabled(true);
                                 btn_login.setText("Login");
                             }
 
                         }
-                        else{
-                            Toast.makeText(getApplicationContext(),"Username tidak ada!", Toast.LENGTH_SHORT).show();
-                            btn_login.setEnabled(true);
-                            btn_login.setText("Login");
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
                         }
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-
-
-
-
+                    });
+                }
             }
         });
 
@@ -139,5 +135,27 @@ public class LoginAct extends AppCompatActivity {
                 doubleBackToExitPressedOnce=false;
             }
         }, 2000);
+    }
+
+    public boolean validation(){
+        boolean valid = true;
+        xusername = findViewById(R.id.xusername);
+        xpass = findViewById(R.id.xpass);
+        String username = xusername.getText().toString();
+        final String password = xpass.getText().toString();
+        if (username.isEmpty() || username.length() < 3) {
+            xusername.setError("at least 3 characters");
+            xusername.requestFocus();
+            valid = false;
+        }else if (password.isEmpty()){
+            xpass.setError("Please Insert Password!");
+            xpass.requestFocus();
+            valid = false;
+        }else {
+            xusername.setError(null);
+            xpass.setError(null);
+        }
+
+        return valid;
     }
 }
